a9a25e273895529eeebe82cd2857bc4f2c178825
Fatalwgx <nothingfatalleo@gmail.com>
2022-03-22 21:08:10 +0300

    Parametrization added

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,14 @@
 from .pages.base_page import BasePage
 from .pages.product_page import ProductPage
+from selenium.common.exceptions import NoAlertPresentException
+import pytest
 
-link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+base_url = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/'
 
-def test_guest_can_add_product_to_cart(browser):
+
+@pytest.mark.parametrize('promo', ['0', '1', '2', '3', '4', '5', '6', pytest.param('7', marks=pytest.mark.xfail), '8', '9'])
+def test_guest_can_add_product_to_cart(browser, promo):
+    link = f'{base_url}?promo=offer{promo}'
     page = ProductPage(browser, link)
     page.open()
     page.ShouldAddToCart()
