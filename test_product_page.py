04434d92f7eab60497c8db6e9e0e5003db02f8be
Fatalwgx <nothingfatalleo@gmail.com>
2022-03-24 13:27:08 +0300

    skip markers removed

--- test_product_page.py
+++ test_product_page.py
@@ -15,7 +15,6 @@ link1 = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_20
 login_link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
 offer1_link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=offer7'
 
-@pytest.mark.skip
 @pytest.mark.parametrize('promo', ['0', '1', '2', '3', '4', '5', '6', pytest.param('7', marks=pytest.mark.xfail), '8', '9'])
 def test_guest_can_add_product_to_cart(browser, promo):
     link = f'{base_url}?promo=offer{promo}'
